e0cadb7f1c8736131942478065ef171d23996b7d
abii19 <joshiabii19@gmail.com>
2022-06-16 10:22:20 +0545

    Treasury passed to components and created dynamic avatars for each treasury

--- src/pages/treasury/index.tsx
+++ src/pages/treasury/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from "next";
-import Image from "next/image";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Layout from "../../components/layouts/Layout";
 import { Button } from "../../components/shared";
-import * as AvatarImages from "../../assets/images/avatars";
 import * as Icons from "../../assets/icons";
+import TreasuryLists from "../../components/treasury/TreasuryLists";
 
 const Treasury: NextPage = () => {
   const { t } = useTranslation();
@@ -27,86 +26,7 @@ const Treasury: NextPage = () => {
               <Button title="See Archived Safe" EndIcon={Icons.TrashIcon} />
             </div>
           </div>
-          <div className="grid grid-cols-3 gap-6">
-            <div className="p-6 bg-background-secondary rounded">
-              <Image
-                src={AvatarImages.Avatar2}
-                layout="fixed"
-                width={48}
-                height={48}
-              />
-              <div className="flex flex-col gap-y-4 mt-6">
-                <div className="text-subtitle text-content-primary font-semibold">
-                  Zebec Safe
-                </div>
-                <div className="flex gap-x-3 items-center">
-                  <div className="flex gap-x-1.5 items-center text-content-primary">
-                    <Icons.UserGroupIcon className="text-base" />
-                    <div>120 Owners</div>
-                  </div>
-                  <div className="flex gap-x-1.5 items-center text-content-primary">
-                    <Icons.NotebookIcon className="text-base" />
-                    <div>1AdXF3...DuV15</div>
-                    <div className="w-7 h-7 grid place-content-center border border-outline rounded-full cursor-pointer">
-                      <Icons.CopyIcon className="text-base" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="p-6 bg-background-secondary rounded">
-              <Image
-                src={AvatarImages.Avatar3}
-                layout="fixed"
-                width={48}
-                height={48}
-              />
-              <div className="flex flex-col gap-y-4 mt-6">
-                <div className="text-subtitle text-content-primary font-semibold">
-                  Ricks Safe
-                </div>
-                <div className="flex gap-x-3 items-center">
-                  <div className="flex gap-x-1.5 items-center text-content-primary">
-                    <Icons.UserGroupIcon className="text-base" />
-                    <div>120 Owners</div>
-                  </div>
-                  <div className="flex gap-x-1.5 items-center text-content-primary">
-                    <Icons.NotebookIcon className="text-base" />
-                    <div>1AdXF3...DuV15</div>
-                    <div className="w-7 h-7 grid place-content-center border border-outline rounded-full cursor-pointer">
-                      <Icons.CopyIcon className="text-base" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="p-6 bg-background-secondary rounded">
-              <Image
-                src={AvatarImages.Avatar4}
-                layout="fixed"
-                width={48}
-                height={48}
-              />
-              <div className="flex flex-col gap-y-4 mt-6">
-                <div className="text-subtitle text-content-primary font-semibold">
-                  Web3 DAO
-                </div>
-                <div className="flex gap-x-3 items-center">
-                  <div className="flex gap-x-1.5 items-center text-content-primary">
-                    <Icons.UserGroupIcon className="text-base" />
-                    <div>120 Owners</div>
-                  </div>
-                  <div className="flex gap-x-1.5 items-center text-content-primary">
-                    <Icons.NotebookIcon className="text-base" />
-                    <div>1AdXF3...DuV15</div>
-                    <div className="w-7 h-7 grid place-content-center border border-outline rounded-full cursor-pointer">
-                      <Icons.CopyIcon className="text-base" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <TreasuryLists />
         </div>
       </div>
     </Layout>
