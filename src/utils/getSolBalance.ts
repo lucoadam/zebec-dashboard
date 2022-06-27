f425ef2eb07ab029d5a19e91d3d654f9c6c048e0
subas shrestha <subasshrestha513@gmail.com>
2022-06-27 12:56:35 +0545

    ZDEV-60 add function to fetch sol balance from wallet

---
+++ src/utils/getSolBalance.ts
@@ -0,0 +1,9 @@
+import { connection } from 'constants/cluster';
+import { PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
+
+export const getSolBalanceOfWallet = async (wallet: String) => {
+    const solBalance = await connection.getBalance(
+        new PublicKey(wallet)
+    );
+    return solBalance/LAMPORTS_PER_SOL;
+}
