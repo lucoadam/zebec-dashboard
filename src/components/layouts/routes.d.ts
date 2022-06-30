a4914f7a2843d0d7edb91623baead50c244b0bd8
subas shrestha <subasshrestha513@gmail.com>
2022-06-30 19:37:27 +0545

    ZDEV-68 Make navbar responsive

---
+++ src/components/layouts/routes.d.ts
@@ -0,0 +1,18 @@
+export interface RouteProps {
+  name: string;
+  path?: string;
+  Icon: React.ElementType;
+  type: "link" | "group";
+  noBackground?: boolean;
+}
+
+export interface RoutesArrayProps extends RouteProps {
+  children?: RouteProps[];
+}
+
+export interface DeviceRoutes {
+  width: number;
+  main: string[];
+  more: string[];
+  menu: string[];
+}
--- src/components/layouts/routes.ts
+++ src/components/layouts/routes.ts
@@ -1,19 +1,7 @@
-import React from "react";
 import * as NavIcons from "../../assets/icons/nav-icons";
+import { DeviceRoutes, RoutesArrayProps } from "./routes.d";
 
-export interface RouteProps {
-  name: string;
-  path?: string;
-  Icon: React.ElementType;
-  type: "link" | "group";
-  noBackground?: boolean;
-}
-
-export interface RoutesArrayProps extends RouteProps {
-  children?: RouteProps[];
-}
-
-export const routes: RoutesArrayProps[] = [
+const routesArray: RoutesArrayProps[] = [
   {
     name: "dashboard",
     path: "/",
@@ -45,22 +33,75 @@ export const routes: RoutesArrayProps[] = [
     type: "link",
   },
   {
-    name: "more",
-    Icon: NavIcons.ThunderIcon,
-    type: "group",
-    children: [
-      {
-        name: "address-book",
-        path: "/address-book",
-        Icon: NavIcons.PhoneBookIcon,
-        type: "link",
-      },
-      {
-        name: "settings",
-        path: "/settings",
-        Icon: NavIcons.GearringIcon,
-        type: "link",
-      },
-    ],
+    name: "address-book",
+    path: "/address-book",
+    Icon: NavIcons.PhoneBookIcon,
+    type: "link",
+  },
+  {
+    name: "settings",
+    path: "/settings",
+    Icon: NavIcons.GearringIcon,
+    type: "link",
   },
 ];
+
+const deviceRoutes: DeviceRoutes[] = [
+  {
+    width: 768,
+    main: [],
+    more: [],
+    menu: [
+      "dashboard",
+      "treasury",
+      "transactions",
+      "yeild-farming",
+      "dca",
+      "address-book",
+      "settings",
+    ],
+  },
+  {
+    width: 1024,
+    main: ["dashboard", "treasury"],
+    more: ["transactions", "yeild-farming", "dca", "address-book", "settings"],
+    menu: [],
+  },
+  {
+    width: 1440,
+    main: ["dashboard", "treasury", "transactions"],
+    more: ["yeild-farming", "dca", "address-book", "settings"],
+    menu: [],
+  },
+  {
+    width: Number.MAX_VALUE,
+    main: ["dashboard", "treasury", "transactions", "yeild-farming", "dca"],
+    more: ["address-book", "settings"],
+    menu: [],
+  },
+];
+
+export const getMainRoutes = (width: number): RoutesArrayProps[] => {
+  const routes =
+    deviceRoutes.find((r) => width < r.width) ||
+    deviceRoutes[deviceRoutes.length - 1];
+  const mainSection = routesArray.filter((r) => routes.main.includes(r.name));
+  const moreSection = routesArray.filter((r) => routes.more.includes(r.name));
+  if (moreSection.length > 0) {
+    mainSection.push({
+      name: "more",
+      Icon: NavIcons.ThunderIcon,
+      type: "group",
+      children: routesArray.filter((r) => routes.more.includes(r.name)),
+    });
+  }
+  return mainSection;
+};
+
+export const getMenuRoutes = (width: number): RoutesArrayProps[] => {
+  const routes =
+    deviceRoutes.find((r) => width < r.width) ||
+    deviceRoutes[deviceRoutes.length - 1];
+  const menuSection = routesArray.filter((r) => routes.menu.includes(r.name));
+  return menuSection;
+};
