f75327e549c4c798106b00519980a324fbffa37d
shraddhabhuju <shraddha.bhuju@gmail.com>
2022-07-08 11:17:43 +0545

    ZDEV-84

--- src/components/transactions/withdraw/steps/ConfirmWithdraw/ConfirmWithdraw.tsx
+++ src/components/transactions/withdraw/steps/ConfirmWithdraw/ConfirmWithdraw.tsx
@@ -1,8 +1,8 @@
 import React, { FC, Fragment, useRef, useState } from "react";
 import { useTranslation } from "next-i18next";
-import { Button, IconButton, InputField, Modal } from "components/shared";
-import * as Icons from "assets/icons";
+import { Button} from "components/shared";
 import { withdrawProps } from "../../data";
+import { confirmWithdrawStep, confirmWithdrawStepsList } from "./data";
 
 
 const ConfirmWithdraw: FC<withdrawProps> = ({setCurrentStep,withdrawAmount,setWithdrawAmount}) => {
@@ -12,21 +12,19 @@ const ConfirmWithdraw: FC<withdrawProps> = ({setCurrentStep,withdrawAmount,setWi
             <div className="text-content-primary text-subtitle pb-4">
                 {t("withdraw.sure-you-want-to-withdraw")} 101 SOL?
             </div>
-            <div className="bg-background-tertiary text-caption rounded-md p-4">
-                <div className="pb-4 flex text-content-secondary">
-                    <div className="pr-2 "> <Icons.ArrowDownLeft /></div>
-                    {t("withdraw.amount-received")} 20000 SOL
-                </div>
-                <div className="flex text-content-secondary pb-4">
-                    <div className="pr-2"> <Icons.Line /></div>
-                    {t("withdraw.withdrawable-amount")} 20000 SOL
-                </div>
-                <div className="flex text-content-secondary ">
-                    <div className="pr-2"> <Icons.Equals /></div>
-                    <div>{t("withdraw.withdraw-amount")} {withdrawAmount} SOL</div>
+        
 
-                </div>
-
+            <div className="bg-background-tertiary text-caption rounded-md p-4 ">
+            {confirmWithdrawStepsList.map((step: confirmWithdrawStep, index: number) => (
+            <div
+              className={`${index==confirmWithdrawStepsList.length-1? "flex gap-x-2 text-content-secondary": "withdrawsteps-container flex gap-x-2 pb-4 text-content-secondary" }`}
+              key={`steps-${index}`}
+            >
+              {confirmWithdrawStepsList[index].image}
+                    { confirmWithdrawStepsList[index].text} 20000 SOL
+            </div>
+          ))}
+                
 
 
             </div>
