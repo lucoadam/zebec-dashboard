923e637aad6b79b0e1ee7712af6f6aa35ff93611
abii19 <joshiabii19@gmail.com>
2022-07-11 13:39:38 +0545

    Minor changes updated on address book and formatted files

--- src/components/address-book/IndividualAddressesTableRow.tsx
+++ src/components/address-book/IndividualAddressesTableRow.tsx
@@ -1,29 +1,27 @@
-import React, { FC, Fragment, useRef, useState } from "react";
-import { useTranslation } from "next-i18next";
-import { Button} from "components/shared";
-import * as Icons from "assets/icons";
+import React, { FC, Fragment, useRef, useState } from "react"
+import { useTranslation } from "next-i18next"
+import { Button } from "components/shared"
+import * as Icons from "assets/icons"
 
 interface IndividualAddresesTableRow {
-  index: number;
-  transaction: any;
-  activeDetailsRow: "" | number;
-  handleToggleRow: () => void;
+  index: number
+  transaction: any
+  activeDetailsRow: "" | number
+  handleToggleRow: () => void
 }
 
 const IndividualAddresesTableRow: FC<IndividualAddresesTableRow> = ({
   index,
   transaction,
   activeDetailsRow,
-  handleToggleRow,
+  handleToggleRow
 }) => {
-  const { t } = useTranslation("transactions");
-  
+  const { t } = useTranslation("transactions")
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
 
   function toggleModal() {
     setIsOpen(!isOpen)
-
   }
 
   return (
@@ -32,7 +30,9 @@ const IndividualAddresesTableRow: FC<IndividualAddresesTableRow> = ({
         {/* Table Body Row */}
         <tr className={`flex `}>
           <td className="px-5 py-5 ">
-            <div className="flex items-center w-36 h-14 text-subtitle font-semibold">{transaction.name}</div>
+            <div className="flex items-center w-36 h-14 text-subtitle font-semibold">
+              {transaction.name}
+            </div>
           </td>
           <td className="px-5 py-5 ">
             <div className=" w-36 h-14 flex items-center text-content-contrast">
@@ -47,30 +47,23 @@ const IndividualAddresesTableRow: FC<IndividualAddresesTableRow> = ({
                 startIcon={
                   <Icons.ArrowUpRightIcon className="text-content-contrast" />
                 }
-                onClick={() => {
-
-                }}
+                onClick={() => {}}
               />
-              
-                <Button
-                  title="Delete"
-                  size="small"
-                  startIcon={
-                    <Icons.CrossIcon className="text-content-contrast" />
-                  }
-                  onClick={() => {
 
-                  }}
-                />
-
+              <Button
+                title="Delete"
+                size="small"
+                startIcon={
+                  <Icons.CrossIcon className="text-content-contrast" />
+                }
+                onClick={() => {}}
+              />
             </div>
           </td>
         </tr>
-
-
       </Fragment>
     </>
-  );
-};
+  )
+}
 
-export default IndividualAddresesTableRow;
+export default IndividualAddresesTableRow
--- src/fakedata/index.ts
+++ src/fakedata/index.ts
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-export * from "./treasuries";
-export * from "./incomingTransactions";
-export * from "./outgoingTransactions";
-export * from "./scheduledTransactions";
-export * from "./historyTransactions";
-export * from "./withdrawalTransactions";
-export * from "./individualAddressBook";
-=======
 export * from "./treasuries"
 export * from "./incomingTransactions"
 export * from "./outgoingTransactions"
 export * from "./scheduledTransactions"
 export * from "./historyTransactions"
 export * from "./withdrawalTransactions"
->>>>>>> 8d634ee02572ca00fc700121b702b510dfc0ccb8
+export * from "./individualAddressBook"
