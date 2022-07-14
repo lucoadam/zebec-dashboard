be55684072f92d6c8e6fc961e6ddc52291f2d028
Lucao Adam <lucoadam@gmail.com>
2022-07-14 09:31:48 +0545

    format updates

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -2,7 +2,13 @@ import BigNumber from "bignumber.js"
 BigNumber.config({ DECIMAL_PLACES: 20 })
 
 export const formatCurrency = (amount: number, before = "", fix = 2) => {
-  return before + amount.toFixed(fix).replace(/\d(?=(\d{3})+\.)/g, "$&,")
+  return (
+    before +
+    amount
+      .toFixed(fix + 1)
+      .substring(0, amount.toFixed(fix + 1).length - 1)
+      .replace(/\d(?=(\d{3})+\.)/g, "$&,")
+  )
 }
 
 export const displayExponentialNumber = (amount: number) => {
