05a0192c6827632f01be616d83b0edd99ca2c476
Abishkar Joshi <joshiabii19@gmail.com>
2022-08-31 15:00:18 +0545

    Transactions.d.ts file changed

--- src/components/transactions/transactions.d.ts
+++ src/components/transactions/transactions.d.ts
@@ -1,9 +1,9 @@
 export enum StatusType {
-  SCHEDULED = scheduled,
-  ONGOING = ongoing,
-  CANCELLED = cancelled,
-  PAUSED = paused,
-  COMPLETED = completed
+  SCHEDULED = "scheduled",
+  ONGOING = "ongoing",
+  CANCELLED = "cancelled",
+  PAUSED = "paused",
+  COMPLETED = "completed"
 }
 
 export type TransactionStatusType =
@@ -12,40 +12,3 @@ export type TransactionStatusType =
   | StatusType.CANCELLED
   | StatusType.PAUSED
   | StatusType.COMPLETED
-
-// interface IndividualTransactionType {
-//   id: number
-//   status: string
-//   start_time: number
-//   end_time: number
-//   sender: string
-//   token_mint_address: string
-//   amount: number
-//   name: string
-//   uuid: string
-//   transaction_type: "instant" | "continuous"
-//   transaction_hash: string
-//   remarks: string
-//   pda: string
-//   receiver: string
-//   token: string
-//   id: 0
-//   status: string
-//   start_time: string
-//   end_time: string
-//   sender: string
-//   token_mint_address: string
-//   amount: number
-//   latest_transaction_event: {
-//     id: number
-//     blockchainStatus: string
-//     paused: boolean | null
-//     withdrawn: string | null
-//     withdraw_limit: string | null
-//     paused_at: string | null
-//     paused_amt: string | null
-//     time: string | null
-//     status: string
-//     transaction: number
-//   }
-// }
