bf82c7a06811094443691e40a02d295b5dd122b1
abii19 <joshiabii19@gmail.com>
2022-11-14 10:45:11 +0545

    Apis call resolved on cancel treasury transactions

--- src/features/modals/signModalSlice.ts
+++ src/features/modals/signModalSlice.ts
@@ -2,11 +2,15 @@ import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { fetchTreasuryVaultTransactionsById } from "api"
 import api from "api/api"
 import { AppDispatch, RootState } from "app/store"
+import { constants } from "constants/constants"
+import { fetchTreasuryBalance } from "features/treasuryBalance/treasuryBalanceSlice"
+import { fetchTreasuryVaultBalance } from "features/treasuryBalance/treasuryVaultBalanceSlice"
 import {
   fetchTreasuryPendingTransactions,
   fetchTreasuryTransactionsById,
   fetchTreasuryVaultContinuousTransactionsById,
-  fetchTreasuryVaultInstantTransactionsById
+  fetchTreasuryVaultInstantTransactionsById,
+  updateTreasuryVaultContinuousTransactionsStatus
 } from "features/treasuryTransactions/treasuryTransactionsSlice"
 
 //declare types for state
@@ -30,7 +34,7 @@ export const signTransaction = createAsyncThunk<
 >(
   "signTransaction/signTransaction",
   async ({ uuid }, { dispatch, getState }) => {
-    const { treasury } = getState()
+    const { treasury, signTransaction } = getState()
     if (treasury.activeTreasury?.uuid) {
       const treasury_uuid = treasury.activeTreasury.uuid
       await api.get(`/treasury/${treasury_uuid}/transactions/${uuid}/approve/`)
@@ -44,6 +48,30 @@ export const signTransaction = createAsyncThunk<
         })
       )
       dispatch(toggleSignModal())
+      // After last sign fetch balance
+      if (
+        signTransaction.transaction.approved_by.length + 1 >=
+        treasury.activeTreasury.min_confirmations
+      ) {
+        setTimeout(() => {
+          if (treasury.activeTreasury) {
+            //Treasury Balance
+            dispatch(
+              fetchTreasuryBalance({
+                name: treasury.activeTreasury.name,
+                address: treasury.activeTreasury.treasury_address
+              })
+            )
+            //Treasury Vault Balance
+            dispatch(
+              fetchTreasuryVaultBalance({
+                name: treasury.activeTreasury.name,
+                address: treasury.activeTreasury.treasury_vault_address
+              })
+            )
+          }
+        }, constants.BALANCE_FETCH_TIMEOUT)
+      }
       return
     }
     return
@@ -104,11 +132,11 @@ export const vaultContinuousSignTransaction = createAsyncThunk<
 
 export const vaultContinuousSignTransactionLatestEvent = createAsyncThunk<
   any,
-  { uuid: string; event_id: string },
+  { uuid: string; event_id: string; transaction_hash?: string },
   { dispatch: AppDispatch; state: RootState }
 >(
   "signTransaction/vaultContinuousSignTransactionLatestEvent",
-  async ({ uuid, event_id }, { dispatch, getState }) => {
+  async ({ uuid, event_id, transaction_hash }, { dispatch, getState }) => {
     const { treasury, signTransaction } = getState()
     if (treasury.activeTreasury?.uuid) {
       const treasury_uuid = treasury.activeTreasury.uuid
@@ -121,7 +149,17 @@ export const vaultContinuousSignTransactionLatestEvent = createAsyncThunk<
           uuid: uuid
         })
       )
+      if (transaction_hash) {
+        dispatch(
+          updateTreasuryVaultContinuousTransactionsStatus({
+            treasury_uuid: treasury_uuid,
+            uuid: uuid,
+            transaction_hash: transaction_hash
+          })
+        )
+      }
       dispatch(toggleSignModal())
+      // After last sign fetch transactions after certain timeout
       if (
         signTransaction.transaction.latest_transaction_event.approved_by
           .length +
