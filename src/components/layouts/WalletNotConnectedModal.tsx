1072f01a6e80991115763ac911af9f4a49bdac48
abii19 <joshiabii19@gmail.com>
2022-11-20 10:34:03 +0545

    Sign message modal design reviewd

--- src/components/layouts/WalletNotConnectedModal.tsx
+++ src/components/layouts/WalletNotConnectedModal.tsx
@@ -3,13 +3,14 @@ import { useWalletModal } from "@solana/wallet-adapter-react-ui"
 import { login } from "api"
 import TokenService from "api/services/token.service"
 import { useAppDispatch, useAppSelector } from "app/hooks"
-import { Button, Modal, Toggle } from "components/shared"
+import { Button, IconButton, Modal, Toggle } from "components/shared"
 import { changeSignState } from "features/common/commonSlice"
 import type { NextPage } from "next"
 import { useTranslation } from "next-i18next"
 import { useEffect, useState } from "react"
 import jwt_decode from "jwt-decode"
 import { toSubstring } from "utils"
+import * as Icons from "assets/icons"
 
 interface DecodedTokenProps {
   token_type: string
@@ -83,12 +84,25 @@ const WalletNotConnectedModal: NextPage = () => {
           className="rounded-2xl"
           hasCloseIcon={false}
         >
-          <div className="text-center px-6 py-8">
-            <span className="text-content-primary text-2xl font-semibold">
-              {t("wallet-not-connected.title")}
-            </span>
-            <p className="text-sm space-x-1 text-content-secondary mt-3">
-              {t("wallet-not-connected.description")}
+          <div className="relative px-6 py-8">
+            {/* Cross Button */}
+            {walletObject.connected && !isSigned && (
+              <IconButton
+                icon={<Icons.CrossIcon />}
+                className="absolute right-0 -top-2 w-9 h-9"
+                onClick={() => walletObject.disconnect()}
+              />
+            )}
+            {/* Main Content */}
+            <div className="text-content-primary text-2xl font-semibold text-center">
+              {walletObject.connected && !isSigned
+                ? t("wallet-not-connected.sign-message")
+                : t("wallet-not-connected.title")}
+            </div>
+            <p className="text-sm space-x-1 text-content-secondary mt-3 text-center">
+              {walletObject.connected && !isSigned
+                ? "In order to authenticate yourself please verify your wallet by following the steps below."
+                : t("wallet-not-connected.description")}
             </p>
 
             {walletObject.connected && !isSigned && (
@@ -101,7 +115,7 @@ const WalletNotConnectedModal: NextPage = () => {
                     </div>
                     <div className="flex flex-col gap-y-1">
                       <h4 className="leading-6 font-medium text-sm text-content-primary text-start">
-                        Wallet Connected
+                        Connected wallet
                       </h4>
                       <p className="text-content-secondary bg-background-muted text-sm py-1 px-3 rounded-xl">
                         {toSubstring(
@@ -113,7 +127,7 @@ const WalletNotConnectedModal: NextPage = () => {
                     </div>
                   </div>
                   {/* Divider */}
-                  <div className="h-10 w-px mb-3 bg-background-light"></div>
+                  <div className="h-12 w-px bg-background-light ml-3 transform -translate-y-4"></div>
                   {/* Step 2 */}
                   <div className="flex gap-x-6">
                     <div className="shrink-0 w-6 h-6 rounded-full bg-content-contrast  text-content-primary text-xs grid place-content-center">
@@ -121,15 +135,20 @@ const WalletNotConnectedModal: NextPage = () => {
                     </div>
                     <div className="flex flex-col gap-y-1">
                       <h4 className="leading-6 font-medium text-sm text-content-primary text-start">
-                        Is Ledger Wallet?
+                        Sign {isLedgerWallet ? "transaction" : "message"}
                       </h4>
-                      <p className="text-content-secondary text-sm rounded-xl">
+                      <p className="text-content-secondary text-xs text-start">
+                        {isLedgerWallet
+                          ? "Signed transaction is not sent to blockchain and only serves for ownership verification"
+                          : "Message signed by your wallet only serves for ownership verification"}
+                      </p>
+                      <div className="text-content-secondary text-sm mt-2">
                         <Toggle
-                          text="Toggle to ON for ledger wallet."
+                          text="Are you using Ledger?"
                           onChange={() => setIsLedgerWallet(!isLedgerWallet)}
                           checked={isLedgerWallet}
                         />
-                      </p>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -150,6 +169,15 @@ const WalletNotConnectedModal: NextPage = () => {
                   : handleConnectWallet()
               }
             />
+
+            {walletObject.connected && !isSigned && (
+              <Button
+                className="w-full mt-3"
+                title={`Disconnect`}
+                variant="default"
+                onClick={() => walletObject.disconnect()}
+              />
+            )}
           </div>
         </Modal>
       )}
