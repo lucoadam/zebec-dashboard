97c01e9d302245fba58c8ac97b3b4c1fe4bb6d0a
subas shrestha <subasshrestha513@gmail.com>
2022-12-07 19:12:08 +0545

    Fixes number field for firefox browser Co-authored-by: Lucao Adam <lucoadam@users.noreply.github.com>

--- src/components/send/ContinuousStream.tsx
+++ src/components/send/ContinuousStream.tsx
@@ -858,6 +858,12 @@ export const ContinuousStream: FC<ContinuousStreamProps> = ({
                     step="any"
                     disabled={getValues().enableStreamRate}
                     {...register("amount")}
+                    onKeyDown={(event) => {
+                      const regex = new RegExp(
+                        /(^\d*\.?\d*$)|(Backspace|Tab|Delete|ArrowLeft|ArrowRight)/
+                      )
+                      return !event.key.match(regex) && event.preventDefault()
+                    }}
                   />
                   {!getValues().enableStreamRate && (
                     <Button
